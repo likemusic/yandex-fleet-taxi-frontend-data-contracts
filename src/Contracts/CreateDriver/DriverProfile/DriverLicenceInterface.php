f4597ca78b0d7e850f359d5a9f0476f1d84e4130
Valerij Ivashchenko <likemusicdev@gmail.com>
2019-09-09 01:35:58 +0300

    Initial

---
+++ src/Contracts/CreateDriver/DriverProfile/DriverLicenceInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Likemusic\YandexFleetTaxi\FrontendData\Contracts\CreateDriver\DriverProfile;
+
+interface DriverLicenceInterface
+{
+//    const BIRTH_DATE = 0;
+    const COUNTRY = 'VU-Strana';
+    const EXPIRATION_DATE = 'Date';
+    const ISSUE_DATE = 'Date_2';
+
+    const SERIES = 'prava-seria';
+    const NUMBER = 'prava-nomer';
+}
---
+++ src/Contracts/ProcessingInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Likemusic\YandexFleetTaxi\FrontendData\Contracts;
+
+interface ProcessingInterface
+{
+    const STATUS = 'status';
+    const STATUS_MESSAGE = 'status message';
+}
