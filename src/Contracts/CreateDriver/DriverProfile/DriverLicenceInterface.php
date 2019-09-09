2a3a8577fa29f314bccfc7a4d4b0320610f1e391
Valerij Ivashchenko <likemusicdev@gmail.com>
2019-09-09 12:01:28 +0300

    Fix dates keys.

--- src/Contracts/CreateDriver/DriverProfile/DriverLicenceInterface.php
+++ src/Contracts/CreateDriver/DriverProfile/DriverLicenceInterface.php
@@ -6,8 +6,8 @@ interface DriverLicenceInterface
 {
 //    const BIRTH_DATE = 0;
     const COUNTRY = 'VU-Strana';
-    const EXPIRATION_DATE = 'Date';
-    const ISSUE_DATE = 'Date_2';
+    const EXPIRATION_DATE = 'Date_2';
+    const ISSUE_DATE = 'Date';
 
     const SERIES = 'prava-seria';
     const NUMBER = 'prava-nomer';
