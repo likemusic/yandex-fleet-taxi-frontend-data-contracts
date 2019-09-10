d39aa4b4736c0364e3fff15e70a46b7ebe15770d
Valerij Ivashchenko <likemusicdev@gmail.com>
2019-09-10 23:07:37 +0300

    Delete old unused interfaces.

--- src/Contracts/ProcessingInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Likemusic\YandexFleetTaxi\FrontendData\Contracts;
-
-interface ProcessingInterface
-{
-    const STATUS = 'status';
-    const STATUS_MESSAGE = 'status message';
-}
