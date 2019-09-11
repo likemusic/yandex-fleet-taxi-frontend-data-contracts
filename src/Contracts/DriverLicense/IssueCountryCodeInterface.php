0b04e6efd6f327846fd79a69ab3985caca2b61a3
Valerij Ivashchenko <likemusicdev@gmail.com>
2019-09-11 16:17:44 +0300

    Add IssueCountryCodeInterface.

---
+++ src/Contracts/DriverLicense/IssueCountryCodeInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Likemusic\YandexFleetTaxi\FrontendData\Contracts\DriverLicense;
+
+interface IssueCountryCodeInterface
+{
+    const RUSSIA = 'rus';
+    const BELARUS = 'blr';
+    const KAZAKHSTAN = 'kaz';
+    const KYRGYZSTAN = 'kgz';
+//todo Абхазия, Южная осетия?
+}
