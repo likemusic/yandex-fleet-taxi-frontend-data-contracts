d5d8caea100876b57f3f54a22f032d7744eeb5c1
Valerij Ivashchenko <likemusicdev@gmail.com>
2019-09-22 00:45:35 +0300

    Delete DriverLicenseInterface::SERIES.

--- src/Contracts/DriverLicenseInterface.php
+++ src/Contracts/DriverLicenseInterface.php
@@ -6,7 +6,6 @@ namespace Likemusic\YandexFleetTaxi\FrontendData\Contracts;
 interface DriverLicenseInterface
 {
     const ISSUE_COUNTRY = 'licence_issue_country';
-    const SERIES = 'licence_series';
     const NUMBER = 'licence_number';
     const ISSUE_DATE = 'licence_issue_date';
     const EXPIRATION_DATE = 'licence_expiration_date';
